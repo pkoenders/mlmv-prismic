ce7dab998135fac7c94fe3d51203a497a1dad62c
Peter Koenders <pkoenders@gmail.com>
2021-08-26 05:25:57 +1200

    aa

--- src/components/common/forms/formContactNew.js
+++ src/components/common/forms/formContactNew.js
@@ -89,9 +89,8 @@ const ContactNew = ({ currentLang, location, formData }) => {
 
     fetch(`${currentLang}/${pathName}`, {
       method: 'POST',
-      headers: { 'Content-Type': 'multipart/form-data' },
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encode({
-        'form-name': e.target.getAttribute('form'),
         ...formObj,
         ...queryString,
       }),

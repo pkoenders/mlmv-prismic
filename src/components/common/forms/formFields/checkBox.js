563f3a2bd5f748f93c4604782f4d1b550e3a371e
Peter Koenders <pkoenders@gmail.com>
2021-08-27 00:24:48 +1200

    aa

--- src/components/common/forms/formFields/checkBox.js
+++ src/components/common/forms/formFields/checkBox.js
@@ -7,7 +7,12 @@ const CheckBox = ({ label, input, meta, fieldId }) => {
   return (
     <label className={`${hasValue ? 'touched' : ''}`}>
       {label}
-      <input type="checkbox" id={fieldId} name={label} value="True" />
+      <input
+        type="checkbox"
+        id={fieldId}
+        name={name.replace(/\s/g, '').toLowerCase()}
+        value={label}
+      />
     </label>
   )
 }
